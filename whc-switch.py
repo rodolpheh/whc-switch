2517144e1a51198f411d395d5978fc62b0406a11
Rodolphe HOUDAS <houdas.rodolphe@gmail.com>
2014-04-19 16:33:28 +0200

    Reverting...

--- whc-switch.py
+++ whc-switch.py
@@ -8,7 +8,6 @@ import sys
 # The switch should be connected between a GPIO and the ground.  
 # GPIO __/ __ GND
 fpin = 7
-spin = 11
 
 # The first argument of the script should be the network device to manage
 device = sys.argv[1]
@@ -19,16 +18,16 @@ def loop():
     sleep(600)
 
 # This function set the network depending on the switch state    
-def set_network():
+def set_network(ping=fpin):
   # If switch is on
-  if gpio.input(fpin) and gpio.input(spin):
+  if gpio.input(fpin):
     set_host()
   # If switch is off
   else:
     set_client()
  
 # Set the access point (AP with hostapd) and start gmediarender
-def set_host(pin=spin):
+def set_host(pin=fpin):
   print('')
   reset_client()
   print('Setting host')
@@ -46,7 +45,7 @@ def set_client(pin=fpin):
   system('systemctl start netctl-auto@' + device)
 
 # Reset the access point and stop gmediarender  
-def reset_host(pin=spin):
+def reset_host(pin=fpin):
   print('Resetting host')
   system('systemctl stop gmediarender')
   system('systemctl stop hostapd')
@@ -67,7 +66,6 @@ print("Setting GPIO...")
 gpio.setmode(gpio.BOARD)
 # Set up pin 7 as an input with pull-up resistor
 gpio.setup(fpin, gpio.IN, pull_up_down=gpio.PUD_UP)
-gpio.setup(spin, gpio.IN, pull_up_down=gpio.PUD_UP)
 
 print("Setting wifi accordingly to original state")
 
@@ -75,9 +73,8 @@ set_network();
 
 print("Setting interrupt...")
 
-# Add events on rising edges
-gpio.add_event_detect(fpin, gpio.RISING, callback=set_client, bouncetime=200)
-gpio.add_event_detect(spin, gpio.RISING, callback=set_host, bouncetime=200)
+# Add event on rising and falling edge
+gpio.add_event_detect(fpin, gpio.RISING, callback=set_network, bouncetime=200)
 
 # Run the loop function to keep script running 
 loop() 
