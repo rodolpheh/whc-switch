a19cfd063cc8a01352413efd034934ba18a73fe3
Rodolphe HOUDAS <houdas.rodolphe@gmail.com>
2014-04-19 16:10:05 +0200

    Changing interrupts behaviour

--- whc-switch.py
+++ whc-switch.py
@@ -7,7 +7,8 @@ import sys
 # Pin where the switch is connected.
 # The switch should be connected between a GPIO and the ground.  
 # GPIO __/ __ GND
-spin = 7
+fpin = 7
+spin = 11
 
 # The first argument of the script should be the network device to manage
 device = sys.argv[1]
@@ -18,17 +19,17 @@ def loop():
     sleep(600)
 
 # This function set the network depending on the switch state    
-def set_network(pin=spin):
-  print('')
+def set_network():
   # If switch is on
-  if gpio.input(pin):
+  if gpio.input(fpin) and gpio.input(spin):
     set_host()
   # If switch is off
   else:
     set_client()
  
 # Set the access point (AP with hostapd) and start gmediarender
-def set_host(pin=spin):
+def set_host(pin):
+  print('')
   reset_client()
   print('Setting host')
   system('ip link set up dev ' + device)
@@ -38,13 +39,14 @@ def set_host(pin=spin):
   system('systemctl start gmediarender')
 
 # Connect to an available network  
-def set_client(pin=spin):
+def set_client(pin):
+  print('')
   reset_host()
   print('Setting client')
   system('systemctl start netctl-auto@' + device)
 
 # Reset the access point and stop gmediarender  
-def reset_host(pin=spin):
+def reset_host(pin):
   print('Resetting host')
   system('systemctl stop gmediarender')
   system('systemctl stop hostapd')
@@ -53,7 +55,7 @@ def reset_host(pin=spin):
   system('ip link set down dev ' + device)
 
 # Disconnect from the network  
-def reset_client(pin=spin):
+def reset_client(pin):
   print('Resetting client')
   system('systemctl stop netctl-auto@' + device)
   system('ip addr flush dev ' + device)
@@ -64,16 +66,18 @@ print("Setting GPIO...")
 # Set pin numbering to board numbering
 gpio.setmode(gpio.BOARD)
 # Set up pin 7 as an input with pull-up resistor
+gpio.setup(fpin, gpio.IN, pull_up_down=gpio.PUD_UP)
 gpio.setup(spin, gpio.IN, pull_up_down=gpio.PUD_UP)
 
 print("Setting wifi accordingly to original state")
 
-set_network(spin);
+set_network();
 
 print("Setting interrupt...")
 
-# Add an event on a rising or falling edge
-gpio.add_event_detect(spin, gpio.BOTH, callback=set_network, bouncetime=200)
+# Add events on rising edges
+gpio.add_event_detect(fpin, gpio.RISING, callback=set_client, bouncetime=200)
+gpio.add_event_detect(spin, gpio.RISING, callback=set_host, bouncetime=200)
 
 # Run the loop function to keep script running 
 loop() 
