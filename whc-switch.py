e5f3663185d4c87fb0f42f356158d1722fc2709d
Rodolphe HOUDAS <houdas.rodolphe@gmail.com>
2014-04-21 13:44:22 +0200

    Minor adjustments

--- whc-switch.py
+++ whc-switch.py
@@ -8,7 +8,7 @@ import subprocess
 # Pin where the switch is connected.
 # The switch should be connected between a GPIO and the ground.  
 # GPIO __/ __ GND
-fpin = 7
+spin = 7
 blue_led = 11
 red_led = 12
 
@@ -21,29 +21,33 @@ state = 0
 # Define a function to keep script running
 def loop():
   while True:
-    while !state:
+    while state:
       sleep(1)
       GPIO.output(blue_led, not GPIO.input(blue_led))
-    while state:
+    while not state:
       sleep(1)
       GPIO.output(blue_led, GPIO.HIGH)
       
 # This function set the network depending on the switch state    
-def set_network(pin=fpin):
+def set_network(pin=spin):
+  
+  # Light off the blue led
+  GPIO.output(blue_led, GPIO.LOW)
+  
   global state
   print('\nSwitch state has changed')
-  print('Switch state: ' + str(GPIO.input(fpin)) + ' ; Software state: ' + str(state))
+  print('Switch state: ' + str(GPIO.input(spin)) + ' ; Software state: ' + str(state))
+  
   # If switch is on
-  if not GPIO.input(fpin) and not state:
+  if not GPIO.input(spin) and not state:
     set_host()
-    state = 1
+    
   # If switch is off
-  elif GPIO.input(fpin) and state:
+  elif GPIO.input(spin) and state:
     set_client()
-    state = 0
  
 # Set the access point (AP with hostapd) and start gmediarender
-def set_host(pin=fpin):
+def set_host(pin=spin):
   print('')
   reset_client()
   print('Setting host')
@@ -61,18 +65,20 @@ def set_host(pin=fpin):
     return None
   
   print('Host set')
+  state = 1
 
 # Connect to an available network  
-def set_client(pin=fpin):
+def set_client(pin=spin):
   print('')
   reset_host()
   print('Setting client')
   system('systemctl start netctl-auto@' + device)
   if check_service('netctl-auto@' + device):
     print('Client set')
+    state = 0
 
 # Reset the access point and stop gmediarender  
-def reset_host(pin=fpin):
+def reset_host(pin=spin):
   print('Resetting host')
   system('systemctl stop hostapd')
   system('systemctl stop dhcpd4')
@@ -80,20 +86,22 @@ def reset_host(pin=fpin):
   system('ip link set down dev ' + device)
 
 # Disconnect from the network  
-def reset_client(pin=fpin):
+def reset_client(pin=spin):
   print('Resetting client')
   system('systemctl stop netctl-auto@' + device)
   system('ip addr flush dev ' + device)
   system('ip link set down dev ' + device)
   
 def get_state():
-  state = 0 if GPIO.input(fpin) else 1
+  state = 0 if GPIO.input(spin) else 1
   return state
 
 def check_service(service):
+  
   # Check if service is started
   out = subprocess.Popen('systemctl status ' + service + ' | grep Active: | cut -d":" -f2 | cut -d"(" -f1', stdout=subprocess.PIPE, shell=True).communicate()[0].strip()
-  if out == "inactive":
+  
+  if out in ["inactive", "failed"]:
     GPIO.output(red_led, GPIO.HIGH)
     print('Error on service ' + service + ', check the systemd journal!')
     return 0
@@ -106,21 +114,22 @@ print("Setting GPIO...")
 
 # Set pin numbering to board numbering
 GPIO.setmode(GPIO.BOARD)
+
 # Set up pin 7 as an input with pull-up resistor
-GPIO.setup(fpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(spin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 # Set up outputs
 GPIO.setup(blue_led, GPIO.OUT)
 GPIO.setup(red_led, GPIO.OUT)
 
+# First setting of wifi
 print("Setting wifi accordingly to original state")
-
-state = GPIO.input(fpin)
+state = GPIO.input(spin)
 set_network()
 
-print("Setting interrupt...")
-
 # Add event on rising and falling edge
-GPIO.add_event_detect(fpin, GPIO.RISING, callback=set_network, bouncetime=200)
+print("Setting interrupt...")
+GPIO.add_event_detect(spin, GPIO.RISING, callback=set_network, bouncetime=200)
 
 # Run the loop function to keep script running 
 loop() 
